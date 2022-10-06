ea263b2e1f2e04c40618860f2fd21c1497b30c7d
Fell1p <luofelipe@hotmail.com>
2022-10-05 23:51:38 -0300

    Pagina filmes e estilização

---
+++ film.js
@@ -0,0 +1,9 @@
+let titulo = document.getElementById("titulo");
+let genero;
+let diretor = document.getElementById("diretor");
+let duracao = document.getElementById("duracao");
+let dataDeLancamento = document.getElementById("dataDeLancamento");
+let dataAssistido = document.getElementById("dataAssistido");
+let poster = document.getElementById("poster");
+let nota = document.getElementById("nota");
+let critica = document.getElementById("critica");
