861cdf19f89590e956b03c0e33954bfc1c156418
Fell1p <luofelipe@hotmail.com>
2022-10-06 23:08:32 -0300

    Desenvolvimento inicial do JS

--- film.js
+++ film.js
@@ -1,5 +1,5 @@
 let titulo = document.getElementById("titulo");
-let genero;
+let genero = document.getElementById("genero").checked;
 let diretor = document.getElementById("diretor");
 let duracao = document.getElementById("duracao");
 let dataDeLancamento = document.getElementById("dataDeLancamento");
@@ -7,3 +7,22 @@ let dataAssistido = document.getElementById("dataAssistido");
 let poster = document.getElementById("poster");
 let nota = document.getElementById("nota");
 let critica = document.getElementById("critica");
+let botaoAdicionar = document.querySelector(".botao-adicionar")
+
+let filme = {}
+let listaFilmes = []
+
+botaoAdicionar.addEventListener("click", () => {
+    filme = {
+        titulo: titulo.value,
+        genero: genero.value,
+        diretor: diretor.value,
+        duracao: duracao.value,
+        dataDeLancamento: dataDeLancamento.value,
+        dataAssistido: dataAssistido.value,
+        poster: poster.value,
+        nota: nota.value,
+        critica: critica.value
+    }
+    listaFilmes.push(filme)    
+})
