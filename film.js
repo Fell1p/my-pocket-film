2ee52a6d8dadd81b2c88a175d968651065fda665
Fell1p <luofelipe@hotmail.com>
2022-10-09 22:47:47 -0300

    Finalização do DOM e desenvolvimento dos cards

--- film.js
+++ film.js
@@ -1,103 +1,122 @@
 let titulo = document.getElementById("titulo");
-//let genero = document.getElementById("genero").checked;
 let diretor = document.getElementById("diretor");
-let duracao = document.getElementById("duracao");
-let dataDeLancamento = document.getElementById("dataDeLancamento");
 let dataAssistido = document.getElementById("dataAssistido");
 let poster = document.getElementById("poster");
 let nota = document.getElementById("nota");
 let critica = document.getElementById("critica");
 let botaoAdicionar = document.querySelector(".botao-adicionar")
-let cartao = document.querySelector(".cards")
-
-let filme = {}
+let form = document.querySelector('.caixas')
+let cardsFilmes = document.querySelector('.cards')
 let listaFilmes = []
+let filme = {}
+let listaGenero = ["Terror", "Ficção", "Drama", "Romance", "Comédia", "Ação", "Comédia Romantica", "Suspense", "Animação", "Documentário"]
 let contador = 0
-let estrela = `img src="./img/estrela.png" class="icone-estrela"`
-let lixo = `img src="./img/lixeira.png" class="lixeira" width="15px"`
-let expandir = `img src="./img/aberto.png" width="13px"`
 
-botaoAdicionar.addEventListener("click", () => {
 
-   // window.location.pathname = './cardsFilmes.html' 
+form.addEventListener("submit", function (evento) {
+    evento.preventDefault() //cancela o comportamento padrão de um formulario que seria o recarregamento da página 
+    evento.stopPropagation() //evita que a emissão do evento se propague para outros elementos
+
+    var elementos = document.getElementsByClassName("genero");
+    let genero = []
+
+    for (i = 0; i < elementos.length; i++) {
+        if (elementos[i].checked) {
+            genero.push(listaGenero[i])
+        }
+    }
 
     filme = {
         titulo: titulo.value,
-        //genero: genero.value,
+        genero: genero,
         diretor: diretor.value,
-        duracao: duracao.value,
-        dataDeLancamento: dataDeLancamento.value,
         dataAssistido: dataAssistido.value,
         poster: poster.value,
         nota: nota.value,
         critica: critica.value
     }
+
     listaFilmes.push(filme)
 
-    const divCard = document.createElement("div")
-    divCard.classList.add("card")
-
-    const divCardPoster = document.createElement("div")
-    divCardPoster.classList.add("poster")
-    //let posterFilme = `<img src="${listaFilmes[contador].poster}">`
-    //let divCardPosterImg
-    //divCardPosterImg.innerHTML = posterFilme
-
-    const divCardConteudo = document.createElement("div")
-    divCardConteudo.classList.add("conteudo")
-
-    const tituloFilme = document.createElement("h5")
-    tituloFilme.classList.add("titulo")
-    tituloFilme.innerText = listaFilmes[contador].titulo
-
-    const classificacao = document.createElement("div")
-    classificacao.classList.add("classificacao")
-
-    const notaFilme = document.createElement("p")
-    notaFilme.classList.add("nota")
-    notaFilme.innerText = listaFilmes[contador].nota
-
-    //let iconeEstrela
-    //iconeEstrela.innerHTML = estrela
-
-    const assistidoEm = document.createElement("p")
-    assistidoEm.classList.add("dataAssistido")
-    assistidoEm.innerText = listaFilmes[contador].dataAssistido
-
-    /*const generoFilme = document.createElement("p")
-    generoFilme.classList.add("genero")
-    generoFilme.innerText = listaFilmes[contador].genero*/
-
-    const containerBotoes = document.createElement("div")
-    containerBotoes.classList.add("container-botoes")
-
-    const botoesCard = document.createElement("div")
-    botoesCard.classList.add("botoes-card")
-
-   // let iconeLixeira
-   // iconeLixeira.innerHTML = lixo
-   // iconeLixeira.addEventListener("click", () => {
-   //     listaFilmes.splice(contador, 1)
-   // })
-
-   // let botaoVerMais = document.createElement("button")
-   // botaoVerMais.classList.add("botao-verMais")
-    //botaoVerMais.innerHTML = 
+    adicionarFilme()
 
 
-    //containerBotoes.appendChild(botoesCard)
-    //botoesCard.append(iconeLixeira, botaoVerMais)
-
-    classificacao.append(notaFilme, /*iconeEstrela*/)
-    divCardConteudo.append(tituloFilme, classificacao, assistidoEm, /*generoFilme*/)
-
-    //divCardPoster.appendChild(divCardPosterImg)
-
-    divCard.append(divCardPoster, divCardConteudo, classificacao, botoesCard, containerBotoes)
-
-    cartao.appendChild(divCard)
-
-    contador++
-
-    
 })
+
+function adicionarFilme() {
+
+    cardsFilmes.innerHTML = ''
+    // window.location.pathname = './cardsFilmes.html' 
+
+    for (const filme of listaFilmes) {
+        const divCard = document.createElement("div")
+        divCard.classList.add("card")
+
+        const divCardPoster = document.createElement("div")
+        divCardPoster.classList.add("poster")
+        let posterFilme = `<img src="${filme.poster}" class="img-poster">`
+        divCardPoster.innerHTML = posterFilme
+
+        const divCardConteudo = document.createElement("div")
+        divCardConteudo.classList.add("conteudo")
+
+        const tituloFilme = document.createElement("h5")
+        tituloFilme.classList.add("titulo")
+        tituloFilme.innerText = filme.titulo
+
+        const classificacao = document.createElement("div")
+        classificacao.classList.add("classificacao")
+
+        const iconeEstrela = document.createElement("div")
+        iconeEstrela.classList.add("icone-estrela")
+        let estrela = `<img src="./img/estrela.png" class="estrela">`
+        iconeEstrela.innerHTML = estrela
+
+        const notaFilme = document.createElement("p")
+        notaFilme.classList.add("nota")
+        notaFilme.innerText = filme.nota
+
+        const assistidoEm = document.createElement("p")
+        assistidoEm.classList.add("dataAssistido")
+        assistidoEm.innerText = `Assistido em: ${filme.dataAssistido}`
+
+        const generoFilme = document.createElement("p")
+        generoFilme.classList.add("genero")
+        generoFilme.innerText = `${filme.genero} `
+
+        const containerBotoes = document.createElement("div")
+        containerBotoes.classList.add("container-botoes")
+
+        const botoesCard = document.createElement("div")
+        botoesCard.classList.add("botoes-card")
+
+        const lixeira = document.createElement("button")
+        lixeira.classList.add("lixeira")
+        let iconeLixeira = `<img src="./img/lixeira.png" class="lixeira" width="15px">`
+        lixeira.innerHTML = iconeLixeira
+        lixeira.addEventListener("click", () => {
+            indice = listaFilmes[contador]
+            listaFilmes.splice(indice, 1)
+            adicionarFilme()
+        })
+
+        const botaoVerMais = document.createElement("button")
+        botaoVerMais.classList.add("botao-verMais")
+        let btnVerMais = `<img src="./img/aberto.png" width="13px">`
+        botaoVerMais.innerHTML = btnVerMais
+
+        botoesCard.append(lixeira, botaoVerMais)
+        containerBotoes.appendChild(botoesCard)
+
+        classificacao.append(iconeEstrela, notaFilme)
+        divCardConteudo.append(tituloFilme, classificacao, assistidoEm, generoFilme)
+
+        divCard.append(divCardPoster, divCardConteudo, containerBotoes)
+
+        cardsFilmes.appendChild(divCard)
+
+        contador++
+
+        document.querySelector(".caixas").reset()
+    }
+}
